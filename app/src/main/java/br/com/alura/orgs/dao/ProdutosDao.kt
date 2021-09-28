a3a990e836680b884c1d048049b5b796fdc072f6
Alex Felipe <alexfelipevieira@gmail.com>
2021-09-28 08:40:42 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,20 @@
+package br.com.alura.orgs.dao
+
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto) {
+        produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+
+}
