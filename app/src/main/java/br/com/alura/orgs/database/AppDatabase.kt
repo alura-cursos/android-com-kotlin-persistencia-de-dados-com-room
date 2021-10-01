12a950caacadfee026649b430db507fc3c2ad8d5
Alex Felipe <alexfelipevieira@gmail.com>
2021-10-01 12:41:09 -0300

    2.3

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -1,10 +1,14 @@
-package br.com.alura.orgs.database.dao
+package br.com.alura.orgs.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import br.com.alura.orgs.database.converter.Converters
+import br.com.alura.orgs.database.dao.ProdutoDao
 import br.com.alura.orgs.model.Produto
 
 @Database(entities = [Produto::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun produtoDao(): ProdutoDao
 }
---
+++ app/src/main/java/br/com/alura/orgs/database/converter/Converters.kt
@@ -0,0 +1,18 @@
+package br.com.alura.orgs.database.converter
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+
+class Converters {
+
+    @TypeConverter
+    fun deDouble(valor: Double?) : BigDecimal {
+       return valor?.let { BigDecimal(valor.toString()) } ?: BigDecimal.ZERO
+    }
+
+    @TypeConverter
+    fun bigDecimalParaDouble(valor: BigDecimal?) : Double? {
+        return valor?.let { valor.toDouble() }
+    }
+
+}
