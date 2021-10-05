19d9e0bc96dd3913db431bd6db51489bb28e94f5
Alex Felipe <alexfelipevieira@gmail.com>
2021-10-05 13:02:51 -0300

    3.4

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -9,7 +9,7 @@ import br.com.alura.orgs.database.converter.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
 import br.com.alura.orgs.model.Produto
 
-@Database(entities = [Produto::class], version = 1)
+@Database(entities = [Produto::class], version = 1, exportSchema = true)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun produtoDao(): ProdutoDao
