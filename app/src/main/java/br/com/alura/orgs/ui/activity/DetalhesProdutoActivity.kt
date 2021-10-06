061957531ce5f79c52ea0a5a42e754237b51a9c8
Alex Felipe <alexfelipevieira@gmail.com>
2021-10-06 12:50:41 -0300

    4.2

--- app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,12 +2,16 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import br.com.alura.orgs.R
 import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.alura.orgs.extensions.formataParaMoedaBrasileira
 import br.com.alura.orgs.extensions.tentaCarregarImagem
 import br.com.alura.orgs.model.Produto
+
+private const val TAG = "DetalhesProduto"
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
@@ -24,6 +28,18 @@ class DetalhesProdutoActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_detalhes_produto_remover -> {
+                Log.i(TAG, "onOptionsItemSelected: remover")
+            }
+            R.id.menu_detalhes_produto_editar -> {
+                Log.i(TAG, "onOptionsItemSelected: editar")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun tentaCarregarProduto() {
