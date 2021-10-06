5a2c41c89d7ce1ebe3b3576af54e69a45ec34ee6
Alex Felipe <alexfelipevieira@gmail.com>
2021-10-06 09:12:26 -0300

    4.1

--- app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,6 +2,8 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import br.com.alura.orgs.R
 import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.alura.orgs.extensions.formataParaMoedaBrasileira
 import br.com.alura.orgs.extensions.tentaCarregarImagem
@@ -17,6 +19,11 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         tentaCarregarProduto()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun tentaCarregarProduto() {
