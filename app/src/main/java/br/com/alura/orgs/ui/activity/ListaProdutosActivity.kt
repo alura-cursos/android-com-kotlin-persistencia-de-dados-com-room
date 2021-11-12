fdf34444dc386133d80e2ed32456debb34035538
Alex Felipe <alexfelipevieira@gmail.com>
2021-11-12 08:27:07 -0300

    lidando com exceptions

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,12 +3,14 @@ package br.com.alura.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import kotlinx.coroutines.*
+import java.lang.IllegalArgumentException
 
 private const val TAG = "ListaProdutos"
 
@@ -30,8 +32,20 @@ class ListaProdutosActivity : AppCompatActivity() {
         super.onResume()
         val db = AppDatabase.instancia(this)
         val produtoDao = db.produtoDao()
+        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.i(TAG, "onResume: throwable $throwable")
+            Toast.makeText(
+                this@ListaProdutosActivity,
+                "Ocorreu um problema",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         val scope = MainScope()
-        scope.launch {
+        scope.launch(handler) {
+            MainScope().launch(handler) {
+                throw Exception("lançando exception na coroutine em outro scope")
+            }
+            throw IllegalArgumentException("lançando exception na coroutine")
             val produtos = withContext(Dispatchers.IO) {
                 produtoDao.buscaTodos()
             }
