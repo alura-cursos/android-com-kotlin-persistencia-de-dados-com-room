36d5bc669c6c6527d8c320c20acfe4a10d948192
Alex Felipe <alexfelipevieira@gmail.com>
2021-11-17 11:56:54 -0300

    boas práticas em coroutines

--- app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -7,15 +7,15 @@ import br.com.alura.orgs.model.Produto
 interface ProdutoDao {
 
     @Query("SELECT * FROM Produto")
-    fun buscaTodos() : List<Produto>
+    suspend fun buscaTodos() : List<Produto>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun salva(vararg produto: Produto)
+    suspend fun salva(vararg produto: Produto)
 
     @Delete
-    fun remove(produto: Produto)
+    suspend fun remove(produto: Produto)
 
     @Query("SELECT * FROM Produto WHERE id = :id")
-    fun buscaPorId(id: Long) : Produto?
+    suspend fun buscaPorId(id: Long) : Produto?
 
 }
--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.database.dao.ProdutoDao
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import kotlinx.coroutines.*
 import java.lang.IllegalArgumentException
@@ -20,7 +23,10 @@ class ListaProdutosActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityListaProdutosActivityBinding.inflate(layoutInflater)
     }
-    private val job = Job()
+    private val dao by lazy {
+        val db = AppDatabase.instancia(this)
+        db.produtoDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,38 +37,10 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val db = AppDatabase.instancia(this)
-        val produtoDao = db.produtoDao()
-        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            Log.i(TAG, "onResume: throwable $throwable")
-            Toast.makeText(
-                this@ListaProdutosActivity,
-                "Ocorreu um problema",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        val scope = MainScope()
-        scope.launch(job) {
-            repeat(1000) {
-                Log.i(TAG, "onResume: coroutine está em execução $it")
-                delay(1000)
-            }
-        }
-        scope.launch(handler) {
-            MainScope().launch(handler) {
-                throw Exception("lançando exception na coroutine em outro scope")
-            }
-            throw IllegalArgumentException("lançando exception na coroutine")
-            val produtos = withContext(Dispatchers.IO) {
-                produtoDao.buscaTodos()
-            }
+        lifecycleScope.launch {
+            val produtos = dao.buscaTodos()
             adapter.atualiza(produtos)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        job.cancel()
     }
 
     private fun configuraFab() {
